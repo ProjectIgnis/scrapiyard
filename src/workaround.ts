fbf2a32959cb3dd6a6b03e348ff668480d57a81c
Hatter <sanity.sierota@protonmail.com>
2023-12-09 21:59:53 +0800

    clarify stringify issue

--- src/workaround.ts
+++ src/workaround.ts
@@ -1,5 +1,6 @@
-// lets JSON.stringify handle bigints
-// https://github.com/GoogleChromeLabs/jsbi/issues/30
+// lets JSON.stringify handle bigints (https://github.com/GoogleChromeLabs/jsbi/issues/30)
+// needed because io-ts uses JSON.stringify for error reports (https://github.com/gcanti/io-ts/issues/648)
+
 // eslint-disable-next-line functional/no-expression-statements, functional/immutable-data
 Object.defineProperty(BigInt.prototype, 'toJSON', {
   get() {
