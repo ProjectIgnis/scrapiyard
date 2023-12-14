1f901e11e8f9cb2fb39f9832de9aeca56b31baa2
Hatter <sanity.sierota@protonmail.com>
2023-12-14 16:35:34 +0800

    remove package files

--- src/modules/fp-ts-extended/Reader.ts
+++
@@ -1,9 +0,0 @@
-import * as Rd from 'fp-ts/Reader';
-import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray';
-
-export * from 'fp-ts/Reader';
-
-// Rd.sequenceArray loses RNEA typing, so it needs to be cast back
-export const sequenceRNEA = <R, A>(
-  xs: RNEA.ReadonlyNonEmptyArray<Rd.Reader<R, A>>
-) => Rd.sequenceArray(xs) as Rd.Reader<R, RNEA.ReadonlyNonEmptyArray<A>>;
